6addd2d5ed6e6a879a1d05996f4cea7b002455c0
Tribeoftech <gratefulgrowing333@gmail.com>
2023-07-11 15:35:32 -0500

    api.py

--- task1/api.py
+++ task1/api.py
@@ -1,10 +1,14 @@
-from flask import Flask
+flask import Flask
 
-app = Flask(__name__)
+app = Flask(name)
+
+@app.route('/')
+def home():
+    return 'Welcome to the Flask application!'
 
 @app.route('/api/hello')
 def hello_world():
-return 'Hello, World!'
+    return 'Hello, World!'
 
-if __name__ == '__main__':
-app.run(host='0.0.0.0', port=5252)
+if name == 'main':
+    app.run(host='0.0.0.0', port=5252)
