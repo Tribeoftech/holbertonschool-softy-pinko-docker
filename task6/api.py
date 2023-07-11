3f4c14c3a8cf323a758929987db1bf592c764585
Tribeoftech <gratefulgrowing333@gmail.com>
2023-07-11 15:45:38 -0500

    api.py

---
+++ task6/api.py
@@ -0,0 +1,14 @@
+flask import Flask
+
+app = Flask(name)
+
+@app.route('/')
+def home():
+    return 'Welcome to the Flask application!'
+
+@app.route('/api/hello')
+def hello_world():
+    return 'Hello, World!'
+
+if name == 'main':
+    app.run(host='0.0.0.0', port=5252)
